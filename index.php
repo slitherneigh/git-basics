e63650c52b9ffb78c3e7709c1c1fa9ef6e1a7e15
slitherneigh <slitherneigh@yandex.com>
2015-09-04 21:15:34 +0300

    2

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 <html>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <head>
-	<title></title>
+	<title>Τρομερό ε;;</title>
 </head>
 <body>
-	
+	Gia na doume re paidia
 	
 </body>
 </html>
